6f0fe6fd782fd404c9e0d37437eb7e8a976c92be
marshnil_13 <btech10225.20@bitmesra.ac.in>
2021-11-15 00:29:59 +0530

    final commit

--- app.js
+++ app.js
@@ -16,16 +16,22 @@
 //     }
 // }
 //************************************ Setting Headers *********************************
-const jokes = document.querySelector('#jokes')
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+        const newLi = document.createElement('li');
+        newLi.append(jokeText);
+        jokes.append(newLi);
+}
+button.addEventListener('click', addNewJoke);
 const getDadJoke = async () => {
-    try {
+    try{
         const config = { headers: { Accept: 'application/json' } }
         const res = await axios.get('https://icanhazdadjoke.com/',config);
-        console.log(res.data.joke)
-        const newLi = document.createElement('li');
-        newLi.append(res.data.joke);
-        jokes.append(newLi);
-    } catch (e) {
+        return res.data.joke;
+    } catch(e){
         return "NO JOKES AVAILABLE! SORRY :("
     }
 }
