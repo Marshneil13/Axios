c8fb72f0422c4e12307c75c393cefb0f4eb10e35
marshnil_13 <btech10225.20@bitmesra.ac.in>
2021-11-14 13:47:21 +0530

    final commit

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 //************************************* Axios ***************************************
 // A library for making http requests
-axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-.then(res => {
-    console.log(res.data.ticker.price);
-}).catch(err => {
-    console.log("ERROR!", err)
-})
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+// .then(res => {
+//     console.log(res.data.ticker.price);
+// }).catch(err => {
+//     console.log("ERROR!", err)
+// })
 
-const fetchBitcoinPrice = async () => {
-    try{
-        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-        console.log(res.data.ticker.price);
-    }catch(e){
-        console.log("ERROR!", e)
-    }
-}
+// const fetchBitcoinPrice = async () => {
+//     try{
+//         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//         console.log(res.data.ticker.price);
+//     }catch(e){
+//         console.log("ERROR!", e)
+//     }
+// }
 //************************************ Setting Headers *********************************
+const jokes = document.querySelector('#jokes')
 const getDadJoke = async () => {
-    const config = { headers: {Accept: 'application/json'} }
-    const res = await axios.get('https://icanhazdadjoke.com/',config);
-    console.log(res);
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/',config);
+        console.log(res.data.joke)
+        const newLi = document.createElement('li');
+        newLi.append(res.data.joke);
+        jokes.append(newLi);
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :("
+    }
 }
